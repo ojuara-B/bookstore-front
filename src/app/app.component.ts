a2a311ff0e5987402a02fa9dc40fafadecfc2350
ojuara-B <deividaraujope@gmail.com>
2021-09-27 11:54:54 -0300

    arquivos

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { appendFile } from 'fs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+  
+})
+export class AppComponent {
+  title = 'bookstore-front';
+}
